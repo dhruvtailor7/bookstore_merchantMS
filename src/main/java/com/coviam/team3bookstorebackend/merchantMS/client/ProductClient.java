3f913a225e3faba5426e8b25f96c57afe90ca84c
dhruvtailor7 <32268377+dhruvtailor7@users.noreply.github.com>
2020-04-20 18:52:28 +0530

    Update ProductClient.java

--- src/main/java/com/coviam/team3bookstorebackend/merchantMS/client/ProductClient.java
+++ src/main/java/com/coviam/team3bookstorebackend/merchantMS/client/ProductClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "product", url = "")
+@FeignClient("http://bookstore-product.herokuapp.com/")
 public interface ProductClient {
 
 
