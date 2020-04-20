ad089feaba0d551353499de68d7a1c70817d3787
dhruvtailor7 <32268377+dhruvtailor7@users.noreply.github.com>
2020-04-20 18:37:42 +0530

    Update MerchantMsApplication.java

--- src/main/java/com/coviam/team3bookstorebackend/merchantMS/MerchantMsApplication.java
+++ src/main/java/com/coviam/team3bookstorebackend/merchantMS/MerchantMsApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
 public class MerchantMsApplication {
 
 	public static void main(String[] args) {
